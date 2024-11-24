9ed03b06a27068c2480f15276626476b925e6642
air <23257024@life.hkbu.edu.hk>
2024-11-24 15:44:15 +0800

    version02 OpenAuction don't know how to set the current bidder

--- src/paintings/FixedPriceAuction.java
+++ src/paintings/FixedPriceAuction.java
@@ -1,4 +1,19 @@
 package paintings;
 
-public class FixedPriceAuction {
+import players.Player;
+
+public class FixedPriceAuction extends OpenAuction{
+    public FixedPriceAuction(int artist_id){
+        super(artist_id);
+    }
+
+    @Override
+    public String getType() {
+        return "FixedPriceAuction";
+    }
+
+    @Override
+    public void auction(Player[] players) {
+        super.auction(players);
+    }
 }
--- src/paintings/OpenAuction.java
+++ src/paintings/OpenAuction.java
@@ -1,4 +1,36 @@
 package paintings;
 
-public class OpenAuction {
+import players.Player;
+
+public class OpenAuction extends Painting {
+
+    public OpenAuction(int artist_id){
+        super(artist_id);
+    }
+    @Override
+    public String getType() {
+        return "OpenAuction";
+    }
+
+    @Override
+    public void auction(Player[] players) {
+        boolean deal = false;
+        int turn = 0;
+        do{
+            deal = true;
+            for(int i = 0;i<players.length;i++){
+                if(turn == players.length && deal)
+                    break;
+                int bid = players[i].bid(currentBid,this);
+                if(bid > currentBid){
+                    currentBidder = players[i];
+                    currentBid = bid;
+                    deal = false;
+                    turn = 0;
+                }
+                turn++;
+            }
+        }while(!deal);
+        super.sold();
+    }
 }
